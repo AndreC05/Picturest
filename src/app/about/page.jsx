1fb7e889be819c706a55c83bc6702446bda28a84
volto9 <volto9@yahoo.com>
2024-12-12 09:29:15 +0000

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,1 +1,15 @@
-//about page
+export default function AboutPage() {
+  return (
+    <div className="about">
+      <h2 className="aboutPage">About Us</h2>
+      <p className="p1">
+        On this website, you can find pictures from other users. You can like,
+        post and comment on the specific picture. You might also read other
+        users posts.
+      </p>
+      <p className="p2">
+        You can upload your own pictures and share with other users
+      </p>
+    </div>
+  );
+}
