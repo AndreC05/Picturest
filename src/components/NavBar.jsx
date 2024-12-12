c722bfa4cd847a1e4db1aaaec9f04b43d61fce43
Khushi Sukar <khushisukar234@gmail.com>
2024-12-12 11:24:00 +0000

    postError

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import {
+  ActivityLogIcon,
   HomeIcon,
   QuestionMarkCircledIcon,
   QuestionMarkIcon,
@@ -8,15 +9,16 @@ import React from 'react';
 
 export default function NavBar() {
   return (
-    <div>
-      <Link href="/">
+    <div className='flex flex-row justify-center items-center gap-48 bg-black text-white'>
+      <Link className='p-5' href="/">
         Home
         <HomeIcon />
       </Link>
-      <Link href="/about">
+      <Link className='p-5' href="/about">
         About
         <QuestionMarkCircledIcon />
       </Link>
+      <Link className='p-5' href={"/posts"}>Posts <ActivityLogIcon/> </Link>
     </div>
   );
 }
