a1e1b7cc2d00ccf49d92e58489a46ce5bb506265
volto9 <volto9@yahoo.com>
2024-12-12 14:42:33 +0000

    btn

---
+++ src/components/postDeleteBtn.jsx
@@ -0,0 +1,14 @@
+'use client';
+import { EraserIcon } from '@radix-ui/react-icons';
+
+export default function PostDeleteBtn({ post }) {
+  async function handleDeleteClick() {
+    await handlePostDeleteBtn(post);
+  }
+
+  return (
+    <button title="Delete Post" onClick={() => handleDeleteClick(post)}>
+      <EraserIcon />
+    </button>
+  );
+}
---
+++ src/components/postLikeBtn.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { HeartIcon } from '@radix-ui/react-icons';
+
+export default function PostLikeBtn({ post, handlePostLikeBtn }) {
+  async function handleLikeClick() {
+    await handlePostLikeBtn(post);
+  }
+
+  return (
+    <button title="Like Post" onClick={() => handleLikeClick(post)}>
+      <HeartIcon />
+    </button>
+  );
+}
