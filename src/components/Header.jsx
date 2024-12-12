9a919bd8a03305004a917d9d92fdceafa00ee75b
Andre Da Silva Carreira <andreecarreira@gmail.com>
2024-12-12 15:51:31 +0000

    refracture code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import NavBar from './NavBar';
-import Image from 'next/image';
-import { SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/nextjs';
+import React from "react";
+import NavBar from "./NavBar";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 
 export default function Header() {
   return (
     <div className="flex flex-row ">
-      <Image width={150} height={100} src="/images/picturest.jpg" alt="pic" />
+      <Image
+        width={150}
+        height={100}
+        src="/images/picturest.jpg"
+        alt="Logo pic"
+      />
       <NavBar />
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
     </div>
   );
 }
