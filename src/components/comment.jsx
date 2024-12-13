04755056cf96748ed86459c20b75928bff831834
Khushi Sukar <khushisukar234@gmail.com>
2024-12-13 15:55:02 +0000

    comments form

---
+++ src/components/comment.jsx
@@ -0,0 +1,22 @@
+import { db } from "@/utils/db";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Comments() {
+
+    const { userId } = await auth()
+
+    async function handleComment(formData) {
+        "use server";
+        const comment = formData.get("content")
+        await db.query(`INSERT INTO comments(clerk_id, content, post_id, likes) 
+            VALUES($1, $2)`, [userId, comment, post_id, likes])
+    }
+
+    return (
+        <form action={handleComment}>
+            <input name="content" type="text" placeholder="comment" />
+            <button type="submit" >Comment</button>
+        </form>
+    )
+
+}
--- src/components/postLikeBtn.jsx
+++ src/components/postLikeBtn.jsx
@@ -1,15 +1,20 @@
 'use client';
 
-import { HeartIcon } from '@radix-ui/react-icons';
+import { HeartIcon, HeartFilledIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 
 export default function PostLikeBtn({ post, handlePostLikeBtn }) {
+  
+  const [like, setLike] = useState(false)
+
   async function handleLikeClick() {
     await handlePostLikeBtn(post);
+    setLike(!like)
   }
 
   return (
     <button title="Like Post" onClick={() => handleLikeClick(post)}>
-      <HeartIcon />
+     {like ? <HeartFilledIcon/> : <HeartIcon/>}
     </button>
   );
 }
