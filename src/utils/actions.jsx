d5ba4332ea34ec87a1b171525d8fe4ab90b7207c
ayuub salaad <ay.salaad@gmail.com>
2024-12-13 14:04:48 +0000

    ayuub

--- src/utils/actions.jsx
+++ src/utils/actions.jsx
@@ -1,10 +1,16 @@
 'use server';
 
 import { redirect } from 'next/navigation';
+import { db } from './db';
 
 export async function handleNewPost(params) {
   redirect('/posts/newPost');
 }
 export async function handleEditPost(postId) {
   redirect(`/posts/EditPost?postId=${postId}`);
+}
+export async function handleDeletePost(post) {
+  db.query(`DELETE FROM comments WHERE post_id=${post.id}`)
+  db.query(`DELETE FROM posts WHERE id=${post.id}`)
+  redirect(`/posts`)
 }
