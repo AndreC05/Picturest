94899ab9495532ba833e13b302a18af56f42e89d
Andre Da Silva Carreira <andreecarreira@gmail.com>
2024-12-16 10:38:52 +0000

    Fix comments form and comments display

---
+++ src/components/Comment.jsx
@@ -0,0 +1,26 @@
+import { db } from "@/utils/db";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export default async function Comments({ postId }) {
+  const { userId } = await auth();
+
+  async function handleComment(formData) {
+    "use server";
+    const comment = formData.get("content");
+    await db.query(
+      `INSERT INTO comments(clerk_id, content, post_id) 
+            VALUES($1, $2, $3)`,
+      [userId, comment, postId]
+    );
+
+    revalidatePath(`/posts/${postId}`);
+  }
+
+  return (
+    <form className="p-5" action={handleComment}>
+      <input name="content" type="text" placeholder="comment" />
+      <button type="submit">Comment</button>
+    </form>
+  );
+}
