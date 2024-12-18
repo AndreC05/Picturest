e6517955b156c409ff273fb3cdec44833f79f843
volto9 <volto9@yahoo.com>
2024-12-18 08:39:10 +0000

    form

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,53 @@
 import {
   ActivityLogIcon,
   HomeIcon,
+  ImageIcon,
   PersonIcon,
   QuestionMarkCircledIcon,
   QuestionMarkIcon,
-} from "@radix-ui/react-icons";
-import Link from "next/link";
-import React from "react";
+} from '@radix-ui/react-icons';
+import Link from 'next/link';
+import React from 'react';
 
 export default function NavBar() {
   return (
     <div className="flex flex-row ">
       <div className=" flex flex-row justify-center items-center gap-2 ml-0 sm:gap-8 md:gap-12 lg:gap-28 text-white w-72 text-[10px] md:text-lg sm:w-100% md:w-100%">
-
-        <Link className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9  m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800" href="/">
+        <Link
+          className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9  m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800"
+          href="/"
+        >
           Home
           <HomeIcon className="w-3 md:w-5" />
         </Link>
 
-        <Link className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 p-2 hover:bg-neutral-800 flex flex-row justify-center items-center" href={"/create"}>
+        <Link
+          className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 p-2 hover:bg-neutral-800 flex flex-row justify-center items-center"
+          href={'/create'}
+        >
           Create
+          <ImageIcon className="w-2 sm:w-10" />
         </Link>
-        <Link className=" w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800" href={"/posts"}>
+        <Link
+          className=" w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800"
+          href={'/posts'}
+        >
           Posts <ActivityLogIcon className="w-2 sm:w-10" />
         </Link>
-        <Link className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-5 m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800" href={"/users"}>
+        <Link
+          className="w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-5 m-0 p-2 gap-1 sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800"
+          href={'/users'}
+        >
           User Profile <PersonIcon className="w-3 sm:w-10 " />
         </Link>
-        <Link className=" w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 m-0 p-2 gap-1  sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800" href="/about">
+        <Link
+          className=" w-18 sm:w-20 md:w-28 h-12 sm:h-[103px] py-9 m-0 p-2 gap-1  sm:gap-2 flex flex-row justify-center items-center hover:bg-neutral-800"
+          href="/about"
+        >
           About
           <QuestionMarkCircledIcon className="w-3 sm:w-5" />
         </Link>
       </div>
-      
     </div>
   );
 }
--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,38 +1,38 @@
-'use client';
-import React, { useState } from 'react';
+// 'use client';
+// import React, { useState } from 'react';
 
-export default function UploadImage({ setFile }) {
-  const [state, setState] = useState('');
-  const [selectedFile, setSelectedFile] = useState();
+// export default function UploadImage({ setFile }) {
+//   const [state, setState] = useState('');
+//   const [selectedFile, setSelectedFile] = useState();
 
-  return (
-    <div className="h-[450]bg-[#e9e9e9] rounded-lg">
-      <label className="m-5 flex flex-col justify-centre items-centre cursor-pointer h-[90%] border-[2px] border-gray-300 border-dashed rounded -lg text-gray-600">
-        {!selectedFile ? (
-          <div className="lex items-centre flex-col">
-            {/* <HiArrowUpCircle className="text-[22px]" />  */}
-            <h2 className="font-semibold">Click to Upload</h2>
-          </div>
-        ) : null}
+//   return (
+//     <div className="h-[450]bg-[#e9e9e9] rounded-lg">
+//       <label className="m-5 flex flex-col justify-centre items-centre cursor-pointer h-[90%] border-[2px] border-gray-300 border-dashed rounded -lg text-gray-600">
+//         {!selectedFile ? (
+//           <div className="lex items-centre flex-col">
+//             {/* <HiArrowUpCircle className="text-[22px]" />  */}
+//             <h2 className="font-semibold">Click to Upload</h2>
+//           </div>
+//         ) : null}
 
-        {selectedFile ? (
-          <img
-            src={window.URL.createObjectURL(selectedFile)}
-            width={500}
-            height={500}
-            className="object-contain h-[90%]"
-          />
-        ) : null}
-        <input
-          id="dropzone-file"
-          type="file"
-          className="hidden"
-          onChange={(e) => {
-            setFile(e.target.files[0]);
-            setSelectedFile(e.target.files[0]);
-          }}
-        />
-      </label>
-    </div>
-  );
-}
+//         {selectedFile ? (
+//           <img
+//             src={window.URL.createObjectURL(selectedFile)}
+//             width={500}
+//             height={500}
+//             className="object-contain h-[90%]"
+//           />
+//         ) : null}
+//         <input
+//           id="dropzone-file"
+//           type="file"
+//           className="hidden"
+//           onChange={(e) => {
+//             setFile(e.target.files[0]);
+//             setSelectedFile(e.target.files[0]);
+//           }}
+//         />
+//       </label>
+//     </div>
+//   );
+// }
